8c299e4c814f790bac929a4147fcee5866820ee1
alonp13 <alonp13@gmail.com>
2020-04-16 18:02:44 +0300

    Add classes

--- app/src/main/java/com/example/quick_shop/MarketScreen.java
+++ app/src/main/java/com/example/quick_shop/MarketScreen.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.constraintlayout.solver.widgets.Rectangle;
 public class MarketScreen extends View {
 
     Paint paint;
+    Supermarket mSupermarket;
 
     private void init() {
         paint.setColor(Color.BLACK);
@@ -21,33 +23,23 @@ public class MarketScreen extends View {
     public MarketScreen(Context context) {
         super(context);
         paint = new Paint();
+        init();
     }
 
     public MarketScreen(Context context, AttributeSet attrs) {
         super(context, attrs);
+        paint = new Paint();
         init();
     }
 
     public MarketScreen(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        paint = new Paint();
         init();
     }
 
     @Override
     public void onDraw(Canvas canvas) {
-//        int[][] arr = TempData.arr;
-//        for(int i = 0; i < arr.length;i++) {
-//            for(int j = 0 ; i < arr[0].length ; j++) {
-//
-//            }
-//        }
-
-
-
-        canvas.drawLine(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
-        canvas.drawLine(canvas.getWidth(), 0, 0, canvas.getHeight(), paint);
-
-        Log.d("getWidth",canvas.getHeight()+"");
 
     }
 }
