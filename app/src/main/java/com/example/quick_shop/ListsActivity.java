c7d4b0aa39329e5b17cdc1b583545d108bd64aad
darsam44 <darsam44@gmail.com>
2020-04-16 15:19:37 +0300

    cart start 2

--- app/src/main/java/com/example/quick_shop/ListsActivity.java
+++ app/src/main/java/com/example/quick_shop/ListsActivity.java
@@ -2,13 +2,30 @@ package com.example.quick_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListsActivity extends AppCompatActivity {
-
+    //createListBtn
+    private  Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lists);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.createListBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openListsActivity();
+            }
+        });
+    }
+
+    private void openListsActivity() {
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
     }
 }
