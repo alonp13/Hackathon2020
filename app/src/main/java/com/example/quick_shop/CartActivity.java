c6c00456d84710583ed6b1fd0a0c9514fc4ad7d9
alonp13 <alonp13@gmail.com>
2020-04-18 20:36:46 +0300

    CartActivity - locate the button at the bottom

--- app/src/main/java/com/example/quick_shop/CartActivity.java
+++ app/src/main/java/com/example/quick_shop/CartActivity.java
@@ -12,21 +12,29 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class CartActivity extends AppCompatActivity {
-    private static final String TAG = "CartActivity";
 
+    private static final String TAG = "CartActivity";
+    private Button finishBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Pick Items");
+
         setContentView(R.layout.activity_cart);
         Log.d(TAG, "onCreats: Started.");
         ListView listView = (ListView) findViewById(R.id.listview);
 
         ProductListAdapter adapter = new ProductListAdapter(this, R.layout.adapter_veiw_layout, TempData.products);
         listView.setAdapter(adapter);
-        Button btn = (Button) findViewById(R.id.btnButton);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        finishBtn = new Button(this);
+        finishBtn.setText("Finish");
+
+        listView.addFooterView(finishBtn);
+
+
+        finishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openChooseMarketActivity();
